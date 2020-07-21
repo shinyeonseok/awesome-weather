62a05b36d06e631f21662a57e7a4a8de2f524802
shinyeonseok <shinyeonseok@gmail.com>
2020-07-22 00:26:35 +0900

    first commit

---
+++ js/weather.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// 전역 변수
+const API_KEY = 'ee32fb325d3ebd0b785b47a08aa9cf43';
+// 변수
+const weatherIcon = document.querySelector('#weatherIcon'),
+    weatherTemp = document.querySelector('#weatherTemp'),
+    weatherMain = document.querySelector('#weatherMain'),
+    weatherDesc = document.querySelector('#weatherDesc'),
+    weatherLocation = document.querySelector('#weatherLocation'),
+    notification = document.querySelector('.notification');
+// 옵션
+var options = {
+    enableHighAccuracy: true,
+    timeout: 2000,
+    maximumAge: 0
+};
+  
+// 성공 시
+function success(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    // 패치 사용
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    ).then(function(response){ 
+        //network 정보 => json으로 변경
+        return response.json();
+    }).then(function(json){
+        // 출력
+        weatherIcon.innerHTML = `<img src="img/icon-${json.weather[0].icon}.svg"/>`;
+        weatherTemp.innerHTML = Math.round(json.main.temp) + '℃'; //반올림
+        weatherLocation.innerHTML = `${json.sys.country}, ${json.name}`;
+        weatherMain.innerHTML = json.weather[0].main;
+        weatherDesc.innerHTML = json.weather[0].description;
+    });
+};
+  
+// 실패 시
+function error(err) {
+    notification.innerHTML = `<p> ${err.message} </p>`;
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+  
+navigator.geolocation.getCurrentPosition(success, error, options);
